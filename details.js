54367b2b5717ef1a295ba114091a62fbd68b215b
VSVTeam12 <112758732+VSVTeam12@users.noreply.github.com>
2022-11-18 11:15:54 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,5 +1,6 @@
 module.exports = {
-    token: '5437381422:AAESL4AVB83xYHg3iNcenvDm-JxBSW4mxK0',
-    admins: [5169514861,],
+    token: '5248631376:AAEq92FV2RvrZvt-Yzs5O39s9gQ3-5Y3KgQ',
+    admins: [5169514861,1821521027],
     curr:'Paisa'
 }
+
